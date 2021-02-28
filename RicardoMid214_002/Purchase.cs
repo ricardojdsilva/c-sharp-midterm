75cdb72e0a58af1136159d65a46b6118ca6a83b7
ricardojdsilva <ricardojd@gmail.com>
2021-02-27 16:52:12 -0800

    Midterm csis-1175-002 c#

---
+++ RicardoMid214_002/Purchase.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RicardoMid214_002
+{
+    class Purchase
+    {
+        //fields
+        private int invoice_number =0;
+        private double sales_amout = 0.00;
+        private int qty_items = 0;
+        const int SALES_TAX = 5;
+        //default contructor
+        public Purchase()
+        {
+        }
+
+        public Purchase(int iNumber, double SalesValue, int items)
+        {
+            invoice_number = iNumber;
+            sales_amout = SalesValue;
+            qty_items = items;
+        }
+
+        public double CalculateSalesTax
+        {
+
+            get
+            {
+                return (sales_amout * SALES_TAX) / 100;
+                //return 100.00;
+            }
+
+        }
+
+        public double CalculateShippingCharges
+        {
+
+           
+           get
+            {
+
+                if (qty_items < 3)
+                {
+                    return 3.50;
+
+                }
+                else if (qty_items >= 3 && qty_items <= 6)
+                {
+                    return 5.00;
+                }
+                else if (qty_items > 6 && qty_items <= 10)
+                {
+                    return 7.00;
+                }
+                else if (qty_items > 10)
+                {
+                    return 10.00;
+                }
+                else
+                    return 0.00;
+                
+            }
+
+        }
+
+        public double totalAmout
+        {
+            get
+            {
+                return sales_amout + CalculateShippingCharges + CalculateSalesTax;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Purchase details " +
+              "\nInvoice Number: " + invoice_number +
+              "\nSales amount: " + sales_amout.ToString("C") + //fix 
+              "\nSales Tax: " + SALES_TAX + "%" +
+              "\nSales tax amount: " + CalculateSalesTax.ToString("C") +
+              "\nShipping charges: " + CalculateShippingCharges.ToString("C") +
+               "\nTotal amount : " + totalAmout.ToString("C");
+
+        }
+
+
+
+    }
+}
---
+++ RicardoMid214_002/PurchaseApp.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace RicardoMid214_002
+{
+    class PurchaseApp
+    {
+        static void Main(string[] args)
+        {
+
+            string vNexInvoice = "y";
+
+            while(vNexInvoice == "y")
+            {
+
+
+
+                purchase();
+                Console.WriteLine("Enter next Invoice (Y/N)");
+                vNexInvoice =  Console.ReadLine().ToLower();
+
+
+
+            }
+
+            Console.WriteLine("Press any key to finish the APP");
+            Console.ReadKey();
+
+
+        }
+
+        static void purchase ()
+        {
+
+            int vInvoiceNumber;
+            double vSaleAmout = 0.00;
+            int vNumber_itens_purchase = 0;
+            ;
+            Console.WriteLine("Enter purchase details**************");
+            Console.WriteLine();
+            Console.WriteLine("Enter invoice number :");
+
+            vInvoiceNumber = Convert.ToInt32(Console.ReadLine());
+
+
+            //Check if valid invoice number
+            while (true)
+            {
+                if (vInvoiceNumber < 1000 || vInvoiceNumber > 9000)
+                {
+                    Console.WriteLine("Invalid data entered for invoice"); ;
+                    Console.WriteLine("Enter a invoice number between 1000 and 9000 :");
+                    vInvoiceNumber = Convert.ToInt32(Console.ReadLine());
+                }
+                else
+                {
+
+                    break;
+                }
+            }
+
+            Console.WriteLine("Enter the sales amount :");
+            vSaleAmout = Convert.ToDouble(Console.ReadLine());
+
+            while (true)
+            {
+
+                if (vSaleAmout < 0)
+                {
+                    Console.WriteLine("Invalid data entered for sales amount, please enter a nonnegative value");
+                    vSaleAmout = Convert.ToDouble(Console.ReadLine());
+
+
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+
+            Console.WriteLine("Enter number of items purchased :");
+            vNumber_itens_purchase = Convert.ToInt32(Console.ReadLine());
+
+            Purchase purchase = new Purchase(vInvoiceNumber, vSaleAmout, vNumber_itens_purchase);
+
+            Console.WriteLine(purchase.ToString());
+
+        }
+
+    }
+}
